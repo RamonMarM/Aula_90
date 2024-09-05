e7ce9b29f87cac2288497719f3bff204617367b5
RamonMarM <120236024+RamonMarM@users.noreply.github.com>
2024-09-05 17:05:12 -0300

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from . import main
+from ..models import db, User
 
 
 @main.route('/')
 def index():
+    users = User.query.all()
     return render_template('index.html')
